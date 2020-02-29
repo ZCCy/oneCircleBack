257f05e886d5fc405d7d93efb8b317e0be04f70a
ZCCy <48518443+ZCCy@users.noreply.github.com>
2020-02-29 15:05:35 +0800

    first commit

---
+++ src/main/java/com/qingyou/demo/OneCircleApplication.java
@@ -0,0 +1,13 @@
+package com.qingyou.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneCircleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qingyou/demo/OneCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingyou.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneCircleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
